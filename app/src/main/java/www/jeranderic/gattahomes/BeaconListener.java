b7ce5455dfed26a6121bfb0e2fc3fa44b26cfdca
Jeremy MacArthur <fireynis@gmail.com>
2016-05-14 15:37:31 -0400

    threading

--- app/src/main/java/www/jeranderic/gattahomes/BeaconListener.java
+++ app/src/main/java/www/jeranderic/gattahomes/BeaconListener.java
@@ -9,15 +9,17 @@ import com.estimote.sdk.Region;
 import java.util.List;
 import java.util.UUID;
 
-public class BeaconListener extends BeaconManager implements BeaconManager.RangingListener {
+public class BeaconListener extends BeaconManager implements BeaconManager.RangingListener, Runnable {
 
     public int beaconId;
+    private Region region;
 
     public BeaconListener(Context context) {
         super(context);
         this.beaconId = 0;
         this.setBackgroundScanPeriod(1000, 500);
         this.setRangingListener(this);
+        this.region = new Region("Gatta Home Showcase", UUID.fromString("0C22AC37-4957-55F7-AAF6-9579F324E008"), null, null)
     }
 
     private void setBeaconId(int id) {
@@ -29,5 +31,10 @@ public class BeaconListener extends BeaconManager implements BeaconManager.Rangi
         if (!list.isEmpty()) {
             this.setBeaconId(list.get(0).getMajor());
         }
+    }
+
+    @Override
+    public void run() {
+        this.startRanging(this.region);
     }
 }
