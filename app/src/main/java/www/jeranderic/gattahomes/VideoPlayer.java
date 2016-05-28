65ead6c46178998d3b08d35bb087833691722005
Jeremy MacArthur <fireynis@gmail.com>
2016-05-27 21:04:39 -0400

    Video player works, needs to have intent receive

---
+++ app/src/main/java/www/jeranderic/gattahomes/VideoPlayer.java
@@ -0,0 +1,75 @@
+package www.jeranderic.gattahomes;
+
+import android.app.ProgressDialog;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoPlayer extends AppCompatActivity {
+
+    private MediaController mediaControls;
+    private int position;
+    private VideoView myVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_video_player);
+
+        if (this.mediaControls == null) {
+            this.mediaControls = new MediaController(VideoPlayer.this);
+        }
+
+        this.myVideoView = (VideoView) findViewById(R.id.videoView);
+
+        final ProgressDialog progressDialog = new ProgressDialog(VideoPlayer.this);
+
+        progressDialog.setTitle("Test");
+        progressDialog.setMessage("Loading the video...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        try {
+            this.myVideoView.setMediaController(this.mediaControls);
+            this.myVideoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.two));
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+
+        this.myVideoView.requestFocus();
+        this.myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                progressDialog.dismiss();
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    myVideoView.pause();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("Position", myVideoView.getCurrentPosition());
+        this.myVideoView.pause();
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        position = savedInstanceState.getInt("Position");
+        myVideoView.seekTo(position);
+    }
+
+}
